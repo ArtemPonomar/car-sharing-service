5c95b8636f72cf08ab9e09043342888216215ea5
Yuriy Turchin <turchin.yuriy1@gmail.com>
2023-05-29 14:17:19 +0300

    create Car repository

---
+++ src/main/java/com/example/carsharingservice/repository/CarRepository.java
@@ -0,0 +1,7 @@
+package com.example.carsharingservice.repository;
+
+import com.example.carsharingservice.model.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarRepository extends JpaRepository<Car, Long> {
+}
