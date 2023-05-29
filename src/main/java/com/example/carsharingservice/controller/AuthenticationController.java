58a9a2b99f7900e30a7a051009410057911cf766
a.ponomarenko-at-201540164075 <NF1Y9pEm016SCxSUCNnwBwHKLQD+CU8N9IuhiFm/FsA=>
2023-05-29 18:02:50 +0300

    [User] Created user dtos, repo, service, controller

---
+++ src/main/java/com/example/carsharingservice/controller/AuthenticationController.java
@@ -0,0 +1,19 @@
+package com.example.carsharingservice.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @PostMapping("/register")
+    public void registerNewUser(@RequestBody UserDto userDto) {
+
+    }
+
+    @PostMapping("/login")
+    public void login() {
+
+    }
+}
