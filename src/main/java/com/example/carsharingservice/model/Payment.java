48c68824ab463b1a14320113375b5e368687986e
Skrudj <ssinilov@gmail.com>
2023-05-29 18:00:43 +0300

    fixed payment fields access parameters, moved enums inside

--- src/main/java/com/example/carsharingservice/model/Payment.java
+++ src/main/java/com/example/carsharingservice/model/Payment.java
@@ -1,34 +1,33 @@
 package com.example.carsharingservice.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.math.BigDecimal;
 import java.net.URL;
-import lombok.Data;
 
-@Data
 @Entity
 @Table(name = "payments")
 public class Payment {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int sessionId;
-    int rentalId;
-    URL url;
-    BigDecimal paymentAmount;
-    Type type;
-    Status status;
-}
+    @GeneratedValue()
+    private Long sessionId;
+    private Long rentalId;
+    private URL url;
+    @Enumerated(value = EnumType.STRING)
+    private Type type;
+    @Enumerated(value = EnumType.STRING)
+    private Status status;
 
-enum Status {
-    PENDING,
-    PAID
-}
+    enum Status {
+        PENDING,
+        PAID
+    }
 
-enum Type {
-    PAYMENT,
-    FINE
+    enum Type {
+        PAYMENT,
+        FINE
+    }
 }
