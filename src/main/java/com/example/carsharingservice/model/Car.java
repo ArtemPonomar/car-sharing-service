b83e834cab275c87bc47c27870a53455773b5923
Yuriy Turchin <turchin.yuriy1@gmail.com>
2023-05-29 13:39:18 +0300

    create car model

---
+++ src/main/java/com/example/carsharingservice/model/Car.java
@@ -0,0 +1,31 @@
+package com.example.carsharingservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String model;
+    private String brand;
+    private Integer inventory;
+    private CarType carType;
+    private BigDecimal dailyFee;
+}
+
+enum CarType {
+    SEDAN,
+    SUV,
+    HATCHBACK,
+    UNIVERSAL
+}
