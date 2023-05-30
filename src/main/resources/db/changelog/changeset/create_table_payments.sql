6cfd5cb049cc0976ab598b289b129c92e5af7ae3
Oleksandr Koval <0993079960a@gmail.com>
2023-05-30 17:19:39 +0300

    fixed some mistakes

--- src/main/resources/db/changelog/changeset/create_table_payments.sql
+++ src/main/resources/db/changelog/changeset/create_table_payments.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS payments (
 id BIGINT PRIMARY KEY AUTO_INCREMENT,
 rental_id BIGINT,
-sessionId VARCHAR(255),
+session_id VARCHAR(255),
 url VARCHAR(255),
-paymentAmount DECIMAL(10, 2),
-type VARCHAR(255),
-status VARCHAR(255),
-isDeleted BOOLEAN,
+payment_amount DECIMAL(10, 2),
+type ENUM('PAYMENT', 'FINE') NOT NULL,
+status ENUM('PENDING', 'PAID') NOT NULL,
+deleted BOOLEAN,
 CONSTRAINT fk_payment_rental FOREIGN KEY (rental_id) REFERENCES rental(id)
 );
