6512c424559ab60ab14e11565761ca09895483a8
Міша <64218817+mishok321@users.noreply.github.com>
2023-05-30 18:10:16 +0300

    [Rental] Changed GEH & removed update method from Rental Service (#17)
    * Implemented Rental Entity
    
    * Implemented Rental Repositpory
    
    * Implemented basic Rental Service
    
    * Implemented all Rental functions & added small fixes
    
    * fixed checkstyle errors
    
    * Refactored rental mapper & added new endpoint
    
    * Removed update method from Rental Service & changed GEH
    
    * Removed confilt resolver construction
    
    ---------
    
    Co-authored-by: Artem Grunin <98549826+ArtemGrunin@users.noreply.github.com>

--- src/main/java/com/example/carsharingservice/controller/CarSharingGlobalExceptionHandler.java
+++ src/main/java/com/example/carsharingservice/controller/CarSharingGlobalExceptionHandler.java
@@ -3,19 +3,27 @@ package com.example.carsharingservice.controller;
 import com.example.carsharingservice.dto.exception.NoCarsAvailableExceptionDto;
 import com.example.carsharingservice.exception.NoCarsAvailableException;
 import java.time.LocalDateTime;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class CarSharingGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    public static final String NO_CARS_AVAILABLE_EXCEPTION_TEXT
+            = "This car is currently unavailable for rental.";
+
     @ExceptionHandler(NoCarsAvailableException.class)
-    public NoCarsAvailableExceptionDto handleNoCarsAvailableException(
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<NoCarsAvailableExceptionDto> handleNoCarsAvailableException(
             NoCarsAvailableException ex
     ) {
         NoCarsAvailableExceptionDto noCarsAvailableExceptionDto = new NoCarsAvailableExceptionDto();
-        noCarsAvailableExceptionDto.setMessage(ex.getMessage());
+        noCarsAvailableExceptionDto.setMessage(NO_CARS_AVAILABLE_EXCEPTION_TEXT);
         noCarsAvailableExceptionDto.setTimestamp(LocalDateTime.now());
-        return noCarsAvailableExceptionDto;
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(noCarsAvailableExceptionDto);
     }
 }
