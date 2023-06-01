e02e77c4c2b508bf3ce0576b674e2d22a1933fd7
Artem Ponomarenko <37250267+ArtemPonomar@users.noreply.github.com>
2023-06-01 01:44:19 -0700

    [scheduler] Added cron job to RentalServiceImpl that checks for overdue rentals and sends message so subscribers (#24)
    Co-authored-by: a.ponomarenko-at-201540164075 <NF1Y9pEm016SCxSUCNnwBwHKLQD+CU8N9IuhiFm/FsA=>

--- src/main/java/com/example/carsharingservice/repository/RentalRepository.java
+++ src/main/java/com/example/carsharingservice/repository/RentalRepository.java
@@ -1,9 +1,19 @@
 package com.example.carsharingservice.repository;
 
 import com.example.carsharingservice.model.Rental;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RentalRepository extends JpaRepository<Rental, Long> {
+
+    @Query("SELECT r FROM Rental r "
+            + "JOIN FETCH r.car "
+            + "JOIN FETCH r.user "
+            + "WHERE r.actualReturnDate IS NULL "
+            + "AND r.returnDate < (:now)")
+    List<Rental> findOverdueRentals(LocalDateTime now);
 }
