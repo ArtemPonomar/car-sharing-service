77f51c928121eb78f75e91b88929d08e2b63b613
Artem Grunin <artem.grunin.2006@gmail.com>
2023-06-01 22:25:08 +0300

    refactored liquidbase on docker VM

--- src/main/resources/db/changelog/changeset/create_table_payments.sql
+++ src/main/resources/db/changelog/changeset/create_table_payments.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS payments (
-id BIGINT AUTO_INCREMENT PRIMARY KEY,
-rental_id BIGINT,
-session_id VARCHAR(255),
-url VARCHAR(255),
-payment_amount DECIMAL(10, 2),
-type VARCHAR(255) NOT NULL,
-status VARCHAR(255) NOT NULL,
-CONSTRAINT fk_payment_rental FOREIGN KEY (rental_id) REFERENCES rental(id)
+                          payment_amount decimal(38,2) DEFAULT NULL,
+                          id bigint NOT NULL AUTO_INCREMENT,
+                          rental_id bigint DEFAULT NULL,
+                          session_id varchar(255) DEFAULT NULL,
+                          status varchar(255) DEFAULT NULL,
+                          type varchar(255) DEFAULT NULL,
+                          url longtext,
+                          PRIMARY KEY (id),
+                          UNIQUE KEY UK_6b9oj6np97hxx2k68lir605ml (rental_id),
+                          CONSTRAINT FKsd5h2fdxq0uurg1m3jebdl6xd FOREIGN KEY (rental_id) REFERENCES rental (id)
 );
-
