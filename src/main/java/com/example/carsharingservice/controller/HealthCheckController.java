3c5ba1799826a17ab941dd9245c2cbd85b8fd9c2
Artem Grunin <artem.grunin.2006@gmail.com>
2023-06-02 00:43:38 +0300

    huh ebat rabotaet!

--- src/main/java/com/example/carsharingservice/controller/HealthCheckController.java
+++ src/main/java/com/example/carsharingservice/controller/HealthCheckController.java
@@ -1,5 +1,6 @@
 package com.example.carsharingservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
     @GetMapping("/health")
+    @Operation(summary = "Health check controller")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("Application is running");
     }
