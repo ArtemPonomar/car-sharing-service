a7148757cecfa5634ef4b5427916c18a52eccf19
Oleksandr <89537914+sanya-1xx8@users.noreply.github.com>
2023-06-01 13:33:38 +0300

    [payment handler] added PaymentHandlerStrategy (#25)
    * added PaymentHandlerStrategy
    
    * added calculatePaymentAmount and getPaymentByUserId
    
    * fixed habndler
    
    * added handler for Type.PAYMENT
    
    * fixed checkstyle mistakes

---
+++ src/main/java/com/example/carsharingservice/service/impl/FinePaymentHandler.java
@@ -0,0 +1,30 @@
+package com.example.carsharingservice.service.impl;
+
+import com.example.carsharingservice.model.Payment;
+import com.example.carsharingservice.model.Rental;
+import com.example.carsharingservice.service.PaymentHandler;
+import java.math.BigDecimal;
+import java.time.Duration;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FinePaymentHandler implements PaymentHandler {
+    private static final BigDecimal FINE_MULTIPLIER = BigDecimal.valueOf(1.4);
+
+    @Override
+    public BigDecimal calculateTotalAmount(Rental rental) {
+        BigDecimal days = BigDecimal.valueOf(Duration.between(
+                rental.getRentalDate(), rental.getReturnDate()).toDays());
+        BigDecimal overdueDays = BigDecimal.valueOf(
+                Duration.between(rental.getReturnDate(),
+                        rental.getActualReturnDate()).toDays());
+        BigDecimal rentalPayment = days.multiply(rental.getCar().getDailyFee());
+        return rental.getCar().getDailyFee().multiply(FINE_MULTIPLIER).multiply(overdueDays)
+                .add(rentalPayment);
+    }
+
+    @Override
+    public boolean isApplicable(Payment.Type type) {
+        return type.equals(Payment.Type.FINE);
+    }
+}
