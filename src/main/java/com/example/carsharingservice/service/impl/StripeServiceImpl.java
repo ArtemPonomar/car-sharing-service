6fafd226af4f3bd9cae7f2fc99435f3e635553e3
Yuriy Turchin <turchin.yuriy1@gmail.com>
2023-06-01 14:36:38 +0300

    create endpoint for payment

--- src/main/java/com/example/carsharingservice/service/impl/StripeServiceImpl.java
+++ src/main/java/com/example/carsharingservice/service/impl/StripeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.carsharingservice.service.impl;
 
+import com.example.carsharingservice.model.Payment;
 import com.example.carsharingservice.service.StripeService;
 import com.stripe.Stripe;
 import com.stripe.param.checkout.SessionCreateParams;
@@ -10,16 +11,17 @@ import org.springframework.stereotype.Service;
 public class StripeServiceImpl implements StripeService {
     @Value("${STRIPE_SECRET_KEY}")
     private String stripeSecretKey;
+    public final String PAYMENT_URL = "http://localhost:8080/payments";
 
     @Override
-    public SessionCreateParams createPaymentSession(Long rentalId) {
+    public SessionCreateParams createPaymentSession(Long rentalId, Payment.Type type) {
         Stripe.apiKey = stripeSecretKey;
 
         SessionCreateParams.Builder builder = new SessionCreateParams.Builder();
         builder.addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD);
         builder.setMode(SessionCreateParams.Mode.PAYMENT);
-        builder.setSuccessUrl("http://localhost:8080/cars");
-        builder.setCancelUrl("http://localhost:8080/rental");
+        builder.setSuccessUrl(PAYMENT_URL + "?session_id={CHECKOUT_SESSION_ID}");
+        builder.setCancelUrl(PAYMENT_URL + "/cancel");
         builder.addLineItem(
                 new SessionCreateParams.LineItem.Builder()
                         .setPriceData(
